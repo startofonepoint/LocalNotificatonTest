9f4276df41a91ef5da8828c2ec14996905a05055
startofonepoint <startofonpoint@gmail.com>
2015-04-02 15:39:52 +0900

    First Commit

--- LocalNotificationTest/AppDelegate.swift
+++ LocalNotificationTest/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let types:UIUserNotificationType = UIUserNotificationType.Alert|UIUserNotificationType.Badge|UIUserNotificationType.Sound
+        let settings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes: types, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
         return true
     }
 
@@ -41,6 +44,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        application.applicationIconBadgeNumber = 0
+    }
 }
 
--- LocalNotificationTest/ViewController.swift
+++ LocalNotificationTest/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var sendPhoto: UIButton!
+    @IBOutlet var sendPush: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sendPhoto.layer.cornerRadius = 10
+        sendPush.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendLocalPush(sender: AnyObject){
+        var localNoti:UILocalNotification = UILocalNotification()
+        localNoti.alertAction = "iOS8에서 Localpush 테스트중"
+        localNoti.alertBody = "LocalNotificationTest 알림"
+        localNoti.fireDate = NSDate(timeIntervalSinceNow: 30)
+        localNoti.soundName = UILocalNotificationDefaultSoundName
+        localNoti.applicationIconBadgeNumber = 1
+        var thisApplication = UIApplication.sharedApplication()
+        thisApplication.scheduleLocalNotification(localNoti)
+
+    }
+    
+    @IBAction func callActivitiControll(sender: AnyObject) {
+        let items:Array = ["Hello World!!"]
+        let activityView:UIActivityViewController = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        self.presentViewController(activityView, animated: true, completion: nil)
+    }
 
 }
 
